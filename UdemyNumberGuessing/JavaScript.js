ab50816f4fda8440aea0c621950e7b0195d3eb8f
Amy Thompson <amythompson9@gmail.com>
2018-06-14 20:17:04 -0400

    finished game

--- UdemyNumberGuessing/JavaScript.js
+++ UdemyNumberGuessing/JavaScript.js
@@ -1,1 +1,31 @@
-﻿
+﻿//create seecret number
+var secretNumber = 4;
+
+//ask user for guess
+var guess = prompt("What is the secret number?");
+
+//check if guess is right
+//this won't work because you need a number, but are getting a string
+//if (guess === secretNumber) {
+//    alert("YOU GOT IT RIGHT!!")
+//}
+
+if (Number(guess) === secretNumber) {
+    alert("YOU GOT IT RIGHT!!")
+}
+
+//else {
+//    alert("Wrong.")
+//}
+
+else if (Number(guess) > secretNumber) {
+    alert("Too high. Guess again!")
+}
+
+else if (Number(guess) < secretNumber) {
+    alert("Too low. Guess again!")
+}
+
+else {
+    alert("Not a number.")
+}
